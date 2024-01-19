172a2639d30bf360e0fbf641a222816af5a37618
Vinícius Silva <vinicius_vsom@outlook.com>
2024-01-19 18:23:19 +0000

    feat: new css pix code

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ include("firebaseRDB.php");
 
 $db = new firebaseRDB($databaseURL);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -48,12 +50,11 @@ $db = new firebaseRDB($databaseURL);
         }
     </style>
 </head>
-<body>
-    <!-- <a href="add.php"><button>ADD DATA</button></a><br><br> -->
 
+<body>
     <h1>CHÁ DE CASA NOVA ISA E VINI 👩🏻‍❤️‍👨🏾</h1>
-    <p>Obrigado por fazer parte dessa nova etapa das nossas vidas, vamos sempre lembrar de vocês !</p>
-    
+    <p>Obrigado por fazer parte dessa nova etapa das nossas vidas. Vamos sempre lembrar de vocês!</p>
+
     <div class="card-container">
         <?php
         $data = $db->retrieve("film");
@@ -63,49 +64,52 @@ $db = new firebaseRDB($databaseURL);
             foreach ($data as $id => $film) {
                 // Exibir anos presenteados apenas se existirem
                 $giftedYears = isset($film['gifted_years']) ? $film['gifted_years'] : [];
-                
-                echo '<div class="card">
-                    <img src="' . $film['thumbnail'] . '" alt="Thumbnail">
-                    <h3>' . $film['title'] . '</h3>';
+
+                echo '<div class="card">';
+                echo '<img src="' . htmlspecialchars($film['thumbnail'], ENT_QUOTES, 'UTF-8') . '" alt="Thumbnail">';
+                echo '<h3>' . htmlspecialchars($film['title'], ENT_QUOTES, 'UTF-8') . '</h3>';
                 echo '<hr>';
                 // Mostra os anos presenteados como uma lista com a classe CSS
-                echo '<div class="gifted-years">' . implode(' 🎁 . ', $giftedYears ) . '</div>';
-
+                echo '<div class="gifted-years">' . implode(' 🎁 ', array_map('htmlspecialchars', $giftedYears)) . '</div>';
                 // Adiciona o link "PRESENTEAR" dentro do IF
                 echo '<a href="edit.php?id=' . $id . '">PRESENTEAR</a>';
-        
-                echo '<button onclick="copyPix('.$id.')">PIX MISTERIOSO</button>
-                    <p id="pixMessage'.$id.'"></p>
-                </div>';
+                echo '<button onclick="copyPixValue()">PIX MISTERIOSO</button>';
+                echo '<span id="pixValue" style="display:none;">111.222.333-99</span>';
+                echo '<span id="copyMessage" style="padding-top:5px;display:none; color: green;">CHAVE PIX COPIADA!</span>';
+                echo '</div>';
             }
         }
         ?>
     </div>
 
     <button id="backToTopBtn" onclick="scrollToTop()">
-        <img src="/img/acima.png">
+        <img src="/img/acima.png" alt="Back to Top">
     </button>
 
     <script>
-        function copyPix(id) {
-            var pixValue = "132.900.986-05"; // Valor do PIX
-            var tempInput = document.createElement("input");
-            document.body.appendChild(tempInput);
-            tempInput.value = pixValue;
-            tempInput.select();
-            document.execCommand("copy");
-            document.body.removeChild(tempInput);
-
-            // Exibir mensagem de sucesso
-            var pixMessage = document.getElementById("pixMessage" + id);
-            pixMessage.innerHTML = "Pix copiado com sucesso";
-        }
+       function copyPixValue() {
+        var pixSpan = document.getElementById("pixValue");
+        var tempTextarea = document.createElement("textarea");
+        tempTextarea.value = pixSpan.textContent;
+        document.body.appendChild(tempTextarea);
+        tempTextarea.select();
+        tempTextarea.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        document.body.removeChild(tempTextarea);
+        var copyMessage = document.getElementById("copyMessage");
+        copyMessage.style.display = "block";
+        setTimeout(function () {
+            copyMessage.style.display = "none";
+        }, 2000); // Esconde a mensagem após 2 segundos
+    }
 
         function scrollToTop() {
             window.scrollTo({ top: 0, behavior: 'smooth' });
         }
 
-        window.onscroll = function() { showBackToTopButton() };
+        window.onscroll = function () {
+            showBackToTopButton()
+        };
 
         function showBackToTopButton() {
             var btn = document.getElementById("backToTopBtn");
@@ -117,4 +121,5 @@ $db = new firebaseRDB($databaseURL);
         }
     </script>
 </body>
+
 </html>
